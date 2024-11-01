c936db9c782ee47ac77c012d68a8d52f0a8aea32
aldement <aldement117@gmail.com>
2024-11-01 22:14:55 +0200

    New pokemon code

--- logic.py
+++ logic.py
@@ -1,4 +1,5 @@
 from random import randint
+from datetime import datetime, timedelta
 import requests
 
 class Pokemon:
@@ -11,6 +12,7 @@ class Pokemon:
         self.hp = randint(5, 100)
         self.power = randint(1, 100)
         self.was_attacked = False  # Новый атрибут для отслеживания состояния атаки
+        self.last_feed_time = datetime.now()
 
         
         self.img = self.get_img()
@@ -81,7 +83,6 @@ class Pokemon:
         self.level += 1
         self.experience = 0
         self.experience_to_next_level += 100  
-        self.hp += 10 
         self.power += 5
         # Повышаем характеристики покемона с каждым уровнем
         #for stat in self.stats:
@@ -112,9 +113,17 @@ class Pokemon:
         #return f"Статистика:\n{stats_info}"
 
     # Метод для кормления покемона (получение опыта)
-    def feed(self):
-        self.gain_experience(50)
-        return f"{self.name} получил 50 опыта!"
+    def feed(self, feed_interval = 20, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = timedelta(seconds=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            self.gain_experience(50)
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp} И {self.name} получил 50 опыта!"
+        else:
+            return f"Следующее время кормления покемона: {current_time+delta_time}"
+        
 
     # Новый метод атаки
     def attack(self, enemy):
@@ -156,12 +165,18 @@ class Fighter(Pokemon):
     def info(self):
         return f"Это покемон-боец"
     
+    def feed(self):
+        return super().feed(feed_interval=10)
+    
     def attack(self, enemy):
         super_power = randint(5,15)   
         self.power += super_power
-        результат = super().attack(enemy)
+        result = super().attack(enemy)
         self.power -= super_power
-        return результат + f"\nБоец применил супер-атаку силой:{super_power} "
+        return result + f"\nБоец применил супер-атаку силой:{super_power} "
+    
+    def feed(self):
+        return super().feed(hp_increase=20)
 
 
 
--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def go(message):
             pokemon = Wizard(message.from_user.username)
         elif chance == 9 or chance == 10:
             pokemon = Fighter(message.from_user.username)
-            
+
         pokemon = Pokemon(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_img())
@@ -58,6 +58,13 @@ def heal_pokemon(message):
     else:
         bot.send_message(message.chat.id, "Вы не являетесь покемоном и не можете быть вылечены.")
 
+@bot.message_handler(commands=['info'])
+def info_pok(message):
+    if message.from_user.username in Pokemon.pokemons.keys():
+        pok = Pokemon.pokemons[message.from_user.username]
+        bot.send_message(message.chat.id, pok.info())
+    else:
+        bot.send_message(message.chat.id, "У вас нет покемона.")
 
 bot.infinity_polling(none_stop=True)
 
